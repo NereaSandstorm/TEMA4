7f36d7d8647235771a73355bf219609d6461f0c6
alu10228446 <alugut446@ieselcaminas.org>
2022-11-04 18:21:56 +0100

    gbrnegjnerjgmn

--- src/main/kotlin/exercicis/Ex4_3_VisRutes_SQLite.kt
+++ src/main/kotlin/exercicis/Ex4_3_VisRutes_SQLite.kt
@@ -7,6 +7,7 @@ import java.awt.EventQueue
 import java.awt.BorderLayout
 import javax.swing.JPanel
 import java.awt.FlowLayout
+import java.sql.DriverManager
 import javax.swing.JComboBox
 import javax.swing.JButton
 import javax.swing.JTextArea
@@ -16,6 +17,10 @@ class Finestra : JFrame() {
 
     init {
         // Sentències per a fer la connexió
+        val url = "jdbc:sqlite:Rutes.sqlite"
+        val con = DriverManager.getConnection(url)
+        val st1 = con.createStatement()
+
 
         defaultCloseOperation = JFrame.EXIT_ON_CLOSE
         setTitle("JDBC: Visualitzar Rutes")
@@ -29,6 +34,16 @@ class Finestra : JFrame() {
 
         val llistaRutes = arrayListOf<String>()
         // Sentències per a omplir l'ArrayList amb el nom de les rutes
+        val sentenciarutesSQL = "SELECT * FROM RUTES"
+        val rsrutes = st1.executeQuery(sentenciarutesSQL)
+
+        while (rsrutes.next()) {
+            llistaRutes.add(rsrutes.getString(2))
+        }
+
+        rsrutes.close()
+        st1.close()
+
 
 
         val combo = JComboBox<String>(llistaRutes.toTypedArray())
@@ -43,12 +58,30 @@ class Finestra : JFrame() {
         combo.addActionListener() {
             // Sentèncis quan s'ha seleccionat un element del JComboBox
             // Han de consistir en omplir el JTextArea
+            val st2 = con.createStatement()
+            val numero: Int = combo.selectedIndex + 1
+            val sentenciapuntsSQL = "SELECT * FROM PUNTS WHERE num_r = '$numero'"
+            val rspunts = st2.executeQuery(sentenciapuntsSQL)
+
+            area.text = ""
+            while (rspunts.next()) {
+                val nom = rspunts.getString(3)
+                val latitud = rspunts.getFloat(4)
+                val longitud= rspunts.getFloat(5)
+                area.text += nom + " (" + longitud + "-" + latitud + ") \n"
+
+            }
+            rspunts.close()
+            st2.close()
 
         }
 
+
+
         eixir.addActionListener(){
             // Sentències per a tancar la connexió i eixir
-
+            con.close()
+            System.exit(0)
         }
     }
 }
---
+++ src/main/kotlin/util/bd/GestionarRutesBD.kt
@@ -0,0 +1,54 @@
+package util.bd
+
+import exercicis.EX3_2.Ruta
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.Statement
+
+
+class GestionarRutesBD {
+    var urlRutes = ""
+    var st1 :Statement
+    var st2 :Statement
+    val con :Connection
+
+    init {
+        this.urlRutes = "jdbc:sqlite:Rutes.sqlite"
+        this.con = DriverManager.getConnection(this.urlRutes)
+        this.st1 = con.createStatement()
+        this.st2 = con.createStatement()
+
+    }
+
+    fun close() {
+        con.close()
+    }
+
+    fun inserir(r: Ruta) {
+        val ultimoNUmeroRutes = "SELECT MAX(num_r) FROM RUTES"
+        val rsruta = st1.executeQuery(ultimoNUmeroRutes)
+        val num_r = rsruta.getInt(1) + 1
+        println(num_r)
+        val nomR = r.nom
+        val desnivell = r.desnivell
+        val desnac = r.desnivellAcumulat
+        val pr= "INSERT INTO RUTES VALUES ($num_r, '$nomR', $desnivell, $desnac)"
+//        st1.executeUpdate("INSERT INTO RUTES VALUES ($num_r, '$nomR', $desnivell, $desnac)")
+        val ultimoNUmeroPunts= "SELECT MAX(num_p) FROM PUNTS"
+        val rspunts = st1.executeQuery(ultimoNUmeroPunts)
+        var num_p = rspunts.getInt(1)
+        for (r in r.llistaDePunts) {
+            num_p++
+            val nom_p = r.nom
+            val latitud = r.coord.latitud
+            val longitud = r.coord.longitud
+
+            val pr2 = "INSERT INTO PUNTS VALUES ($num_r, '$num_p', $nom_p, $latitud, $longitud)"
+            println(pr2)
+        }
+
+
+
+    }
+
+}
