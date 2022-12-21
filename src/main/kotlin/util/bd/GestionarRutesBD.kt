b06a56a343b1fb43ae504f56e4c04168f0f2b250
Nerea <alugut446@ieselcaminas.org>
2022-12-21 09:46:10 +0100

    hecho

--- src/main/kotlin/util/bd/GestionarRutesBD.kt
+++ src/main/kotlin/util/bd/GestionarRutesBD.kt
@@ -6,39 +6,41 @@ import exercicis.EX3_2.Ruta
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.Statement
-
+import java.util.LinkedList
 
 class GestionarRutesBD {
-    var urlRutes = ""
-    var st1 :Statement
-    var st2 :Statement
-    val con :Connection
+    private var url=""
+    var con : Connection
+    var st : Statement
+    var st2 : Statement
+
 
     init {
-        this.urlRutes = "jdbc:sqlite:Rutes.sqlite"
-        this.con = DriverManager.getConnection(this.urlRutes)
-        this.st1 = con.createStatement()
-        this.st2 = con.createStatement()
+        this.url= "jdbc:sqlite:Rutes.sqlite"
+        this.con= DriverManager.getConnection(url)
+        this.st= con.createStatement()
+        this.st2= con.createStatement()
 
-        val sentSQL = "CREATE TABLE IF NOT EXISTS RUTES(" +
-                "num_r INTEGER CONSTRAINT cp_rut PRIMARY KEY, " +
+        val sentql= "CREATE TABLE IF NOT EXISTS RUTES (" +
+                "num_r INTEGER CONSTRAINT cp_numruta PRIMARY KEY, " +
                 "nom_r TEXT, " +
                 "desn INTEGER, " +
                 "desn_ac INTEGER " +
                 ")"
 
-        val sentSQL2 = "CREATE TABLE IF NOT EXISTS PUNTS(" +
+        val sentql2= "CREATE TABLE IF NOT EXISTS PUNTS (" +
                 "num_r INTEGER, " +
                 "num_p INTEGER, " +
                 "nom_p TEXT, " +
-                "latitud real, " +
-                "longitud real, " +
-                "CONSTRAINT cp_pun primary key(num_r, num_p) " +
-                "foreign key (num_r) references RUTES " +
+                "latitud REAL, " +
+                "longitud REAL, " +
+                "CONSTRAINT cp_punts PRIMARY KEY (num_r,num_p), " +
+                "CONSTRAINT ce_arutes FOREIGN KEY (num_r) REFERENCES RUTES" +
                 ")"
 
-        st1.executeUpdate(sentSQL)
-        st2.executeUpdate(sentSQL2)
+        st.executeUpdate(sentql)
+        st.executeUpdate(sentql2)
+
     }
 
     fun close() {
@@ -46,68 +48,74 @@ class GestionarRutesBD {
     }
 
     fun inserir(r: Ruta) {
-        val ultimoNUmeroRutes = "SELECT MAX(num_r) FROM RUTES"
-        val rsruta = st1.executeQuery(ultimoNUmeroRutes)
-        val num_r = rsruta.getInt(1) + 1
-        println(num_r)
-        val nomR = r.nom
-        val desnivell = r.desnivell
-        val desnac = r.desnivellAcumulat
-        val pr= "INSERT INTO RUTES VALUES ($num_r, '$nomR', $desnivell, $desnac)"
-        st1.executeUpdate("INSERT INTO RUTES VALUES ($num_r, '$nomR', $desnivell, $desnac)")
-        val ultimoNUmeroPunts= "SELECT MAX(num_p) FROM PUNTS"
-        val rspunts = st1.executeQuery(ultimoNUmeroPunts)
-        var num_p = rspunts.getInt(1)
+        val sentencia = "SELECT MAX(num_r) FROM RUTES"
+        val rs = st2.executeQuery(sentencia)
+        var numeromax = rs.getInt(1) + 1
+        val nomr = r.nom
+        val desn = r.desnivell
+        val desnac= r.desnivellAcumulat
+        st.executeUpdate("INSERT INTO RUTES VALUES ($numeromax, '$nomr', $desn, $desnac)")
+        var contador = 0
 
-        for (r in r.llistaDePunts) {
-            num_p++
-            val nom_p = r.nom
-            val latitud = r.coord.latitud
-            val longitud = r.coord.longitud
+        for (e in r.llistaDePunts) {
+            contador++
+            val nomp=e.nom
+            val lat = e.coord.latitud
+            val longi = e.coord.longitud
+            st.executeUpdate("INSERT INTO PUNTS VALUES ($numeromax, $contador, '$nomp', $lat, $longi)")
 
-            st2.executeUpdate("INSERT INTO PUNTS VALUES ($num_r, $num_p, '$nom_p', $latitud, $longitud)")
         }
 
-
+        st2.close()
+        st.close()
     }
 
-    fun buscar(i: Int) : Ruta{
-        val buscaRuta = "SELECT *  FROM RUTES WHERE num_r = '$i'"
-        val resultadoR = st1.executeQuery(buscaRuta)
-        var ruta : Ruta
+    fun buscar(e: Int):Ruta {
+        val sentencia = "SELECT * FROM RUTES WHERE num_r = $e"
+        val rs = st.executeQuery(sentencia)
+        val sentencia2 = "SELECT * FROM PUNTS WHERE num_r = $e"
+        val rs2 = st2.executeQuery(sentencia2)
+        var ruta: Ruta
+        var llista= mutableListOf<PuntGeo>()
 
-        val nomR = resultadoR.getString(2)
-        val desnivell = resultadoR.getInt(3)
-        val desnAc= resultadoR.getInt(4)
-        var list= mutableListOf<PuntGeo>()
-
-        val buscaPunts = "SELECT *  FROM PUNTS WHERE num_r = '$i'"
-        val resultadoP = st2.executeQuery(buscaPunts)
-
-        while (resultadoP.next()) {
-            list.add(PuntGeo(resultadoP.getString(3), Coordenadas(resultadoP.getDouble(4), resultadoP.getDouble(5))))
+        while (rs2.next()) {
+            var punt = PuntGeo(rs2.getString(3), Coordenadas(rs2.getFloat(4).toDouble(), rs2.getFloat(5).toDouble()))
+            llista.add(punt)
         }
 
-        ruta = Ruta(nomR, desnivell, desnAc, list)
+        ruta = Ruta(rs.getString(2), rs.getInt(3), rs.getInt(4), llista)
         return ruta
 
     }
 
     fun llistat(): ArrayList<Ruta> {
-        var lista = arrayListOf<Ruta>()
-        val sentencia = "SELECT MAX(num_r) FROM RUTES"
-        val ultimoNumRuta = st1.executeQuery(sentencia)
+        val sentencia = "SELECT * FROM RUTES"
+        val rs = st.executeQuery(sentencia)
+        var llistaRutas= arrayListOf<Ruta>()
 
-        for (i in 1 until ultimoNumRuta.getInt(1) + 1) {
-            lista.add(buscar(i))
 
+        var ruta: Ruta
+
+        while (rs.next()) {
+            var llista= mutableListOf<PuntGeo>()
+            val num_r= rs.getInt(1)
+
+            val sentencia2 = "SELECT * FROM PUNTS WHERE num_r = $num_r"
+            val rs2 = st2.executeQuery(sentencia2)
+            while (rs2.next()) {
+                var punt = PuntGeo(rs2.getString(3), Coordenadas(rs2.getFloat(4).toDouble(), rs2.getFloat(5).toDouble()))
+                llista.add(punt)
+            }
+            ruta = Ruta(rs.getString(2), rs.getInt(3), rs.getInt(4), llista)
+            llistaRutas.add(ruta)
         }
-        return lista
+
+        return llistaRutas
     }
 
     fun borrar(e: Int) {
-        st1.executeUpdate("DELETE FROM RUTES WHERE num_r = $e")
-        st2.executeUpdate("DELETE FROM PUNTS WHERE num_r = $e")
-    }
+        st.executeUpdate("DELETE FROM PUNTS WHERE num_r = $e")
+        st.executeUpdate("DELETE FROM RUTES WHERE num_r = $e")
 
+    }
 }
